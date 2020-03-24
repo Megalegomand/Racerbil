84fefd2a9b6d38b10dea4160034cd318d7fea935
Brodersen <phbro19@student.sdu.dk>
2020-03-24 12:33:30 +0100

    script til excel export

--- C++/BluetoothTest/BluetoothTest/BluetoothTest.cpp
+++ C++/BluetoothTest/BluetoothTest/BluetoothTest.cpp
@@ -13,13 +13,14 @@ using namespace std;
 
 int main()
 {
+    //Opsætning af wsaData
     WSADATA wsaData;
     int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (iResult != NO_ERROR) {
         wprintf(L"WSAStartup function failed with error: %d\n", iResult);
         return 1;
     }
-
+    // Opsætning af socket
     SOCKET s;
     s = socket(AF_BTH, SOCK_STREAM, BTHPROTO_RFCOMM);
     if (s == INVALID_SOCKET) {
@@ -27,14 +28,14 @@ int main()
         WSACleanup();
         return 1;
     }
-
+    //Opsætning af bth socket adresse
     _SOCKADDR_BTH sbth;
     //memset(&sbth, 0, sizeof(SOCKADDR_BTH));
     sbth.addressFamily = AF_BTH;
     sbth.serviceClassId = RFCOMM_PROTOCOL_UUID;
     sbth.btAddr = BTH_ADDR(0x00066619FD59);
     sbth.port = 0;
-
+    // Tilslutning til bluetooth module
     iResult = connect(s, (sockaddr*)&sbth, sizeof(sbth));
     if (iResult == SOCKET_ERROR) {
         wprintf(L"connect function failed with error: %ld\n", WSAGetLastError());
@@ -44,8 +45,9 @@ int main()
         WSACleanup();
         return 1;
     }
-
+    //Modtagelse af data 
     char recvbuf[200];
+    char sendbuf[1];
     do {
 
         iResult = recv(s, recvbuf, sizeof(recvbuf), 0);
@@ -62,14 +64,15 @@ int main()
             printf("recv failed: %d\n", WSAGetLastError());
 
     } while (iResult > 0);
-
+    // Hvis error opstår output error code, og cleanup wsa
     iResult = closesocket(s);
     if (iResult == SOCKET_ERROR) {
         wprintf(L"closesocket function failed with error: %ld\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
-
+    // hvis ingen fejl output recvbuf, send modtagelse og kør cleanup
+    iResult = send(s, sendbuf, sizeof(sendbuf), 0);
     WSACleanup();
     return 0;
 }
