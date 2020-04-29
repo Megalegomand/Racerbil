1417ad9ca33b07866903eee17ebb99136cfdab82
Simon C. Dockweiler <scdockweiler@gmail.com>
2020-04-29 19:30:26 +0200

    Added shit

--- Assembler/TestBT/TestBT/main.c
+++ Assembler/TestBT/TestBT/main.c
@@ -1,6 +1,19 @@
 #include "USART.h"
 #include "avr/io.h"
 
+#define START			0x08       // TWI start
+#define REP_START		0x10	   // TWI repeat start
+#define MT_SLA_ACK		0x18	   // slave ACK has been received
+#define MT_DATA_ACK		0x28	   // master ACK has been received
+#define NACK			0x38	   // Arbitration lost or NACK
+#define MR_SLA_ACK		0x40       // slave ACK has been received
+#define SLA_W           0b11010000 // slave address + master write
+#define SLA_R           0b11010001 // slave address + master read
+#define READ_ONLY       0x00	   // Read only pointer register mode
+
+enum state {Search, Track, Speed};
+enum state drivestate = Search;
+
 void PWM_init()
 {
 	/*set fast PWM mode with non-inverted output*/
@@ -8,13 +21,21 @@ void PWM_init()
 	DDRD|=(1<<PD7);  /*set OC0 pin as output*/
 }
 
+void TWI_init() {
+	// Set I2C freq to 200kHz
+	TWBR = 8;
+	TWSR = (0 << TWPS1) | (0 << TWPS1);
+}
+
 int main(void){
 	PWM_init();
 	USART_init();
+	TWI_init();
 	char c;
 	while(1){
-		USART_send('E');
 		//c = USART_receive();
+		//USART_send(c);
+		/*
 		if (c == '0') {
 			OCR2 = 0;
 			USART_send(c);
@@ -45,8 +66,78 @@ int main(void){
 		} else if (c == '9') {
 			OCR2 = 255 / 9 * 9;
 			USART_send(c);
+		}*/
+		
+		// Start
+		TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+		
+		while (!(TWCR & (1<<TWINT)));
+		
+		if ((TWSR & 0xF8) != START) {
+			USART_send('1');
+			USART_send(TWSR);
 		}
+		
+		TWDR = SLA_W;
+		TWCR = (1<<TWINT) | (1<<TWEN);
+		
+		while (!(TWCR & (1<<TWINT)));
+		
+		if ((TWSR & 0xF8) != MT_SLA_ACK) {
+			USART_send('2');
+			USART_send(TWSR);
+		}
+		
+		TWDR = 0x3B; // DATA
+		TWCR = (1<<TWINT) | (1<<TWEN);
+		
+		while (!(TWCR & (1<<TWINT)));
+		
+		if ((TWSR & 0xF8) != MT_DATA_ACK) {
+			USART_send('3');
+			USART_send(TWSR);
+		}
+		
+		// Repeat Start
+		TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+		
+		while (!(TWCR & (1<<TWINT)));
+		
+		if ((TWSR & 0xF8) != START) {
+			USART_send('4');
+			USART_send(TWSR);
+		}
+		
+		TWDR = SLA_R;
+		TWCR = (1<<TWINT) | (1<<TWEN);
+		
+		while (!(TWCR & (1<<TWINT)));
+		
+		if ((TWSR & 0xF8) != MR_SLA_ACK) {
+			USART_send('5');
+			USART_send(TWSR);
+		}
+		
+		while (!(TWCR & (1<<TWINT)));
+		
+		USART_send('D');
+		USART_send(TWDR);
+		// Mangler at teste om den skal sende stop
+		
+		// Stop
+		TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+		
+		//while (!(TWCR & (1<<TWINT)));
+		
+		USART_send('S');
+		
+		/*
+		int d=0;
+		for(int i = 0; i < 1000; i++) {
+			for(int i = 0; i < 10000; i++) {
+				d+=1;
+			}
+		}*/
 	}
 	return 0;
 }
-
